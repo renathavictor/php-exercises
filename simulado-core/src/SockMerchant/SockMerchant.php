e5f0dfdf4fa0ccece845e9e022414cdbb76b624a
renatha <renatha.nvictor@gmail.com>
2019-04-16 10:13:27 -0300

    simulado

--- simulado-core/src/SockMerchant/SockMerchant.php
+++ simulado-core/src/SockMerchant/SockMerchant.php
@@ -5,6 +5,15 @@ class SockMerchant
 {
   public static function sockMerchant($socks): int
   {
-    // TODO
+    $uniq  = array_unique($socks);
+    // 1, 2, 3
+    $newArray = array_count_values($socks);
+    // 3, 3, 1
+
+    $count = 0;
+    foreach ($newArray as $value) {
+      $count += floor($value/2);
+    }
+    return $count;
   }
 }
--- simulado-core/src/Vector/ArrayUtil.php
+++ simulado-core/src/Vector/ArrayUtil.php
@@ -11,21 +11,46 @@ class ArrayUtil
 
   public function rotLeft($d)
   {
-    
+    $aux = array_slice($this->array, $d);
+    $back = array_slice($this->array, 0, $d);
+    $result = array_merge($aux, $back);
+    return $result;
   }
 
   public function stats()
   {
-    // TODO
+    $bigger = 0;
+    $smaller = 0;
+    $zero = 0;
+    foreach ($this->array as $value) {
+      if ($value > 0) {
+        $bigger += 1;
+      } else if ($value < 0) {
+        $smaller += 1;
+      } else {
+        $zero += 1;
+      }
+    }
+    $big = \number_format($bigger/sizeof($this->array), 6);
+    $small = \number_format($smaller/sizeof($this->array), 6);
+    $equal = \number_format($zero/sizeof($this->array), 6);
+    return [$big, $small, $equal];
   }
-
+  
   public function miniMaxSum()
   {
-    // TODO
+    $biggest = array_slice($this->array, 1, 4);
+    $smallest = array_slice($this->array, 0, 4);
+    return [array_sum($smallest), array_sum($biggest)];
   }
-
+  
   public function highestFrequency()
   {
-    // TODO
+    $uniq  = array_unique($this->array);
+    $newArray = array_count_values($this->array);
+    $biggest = max($newArray);
+    $highestFrequency = array_keys($newArray, $biggest);
+    return min($highestFrequency);
   }
 }
+
